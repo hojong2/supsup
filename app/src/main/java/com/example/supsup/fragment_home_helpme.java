3b22c69189fc0d5980a164b0910cfd0807810be9
adorable-Kihwan <qkrrlghks20@naver.com>
2022-03-24 16:16:26 +0900

    해주세요 글 누르면 상세 글 나오고 채팅하기 버튼까지

--- app/src/main/java/com/example/supsup/fragment_home_helpme.java
+++ app/src/main/java/com/example/supsup/fragment_home_helpme.java
@@ -2,10 +2,13 @@ package com.example.supsup;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
@@ -15,10 +18,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class fragment_home_helpme extends Fragment {
-
-
     private ListView listview;
     private adapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,8 +34,22 @@ public class fragment_home_helpme extends Fragment {
         adapter.addItem("해주세요 제목1", R.drawable.logo,"해주세요 작성자1");
         adapter.addItem("해주세요 제목2", R.drawable.logo,"해주세요 작성자2");
         adapter.addItem("해주세요 제목3", R.drawable.logo,"해주세요 작성자3");
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(),home_textinfo.class);
+                    startActivity(intent);
+            }
+        });
+
         return v;
     }
+
+
+
+
 }
 
 
---
+++ app/src/main/java/com/example/supsup/home_textinfo.java
@@ -0,0 +1,34 @@
+package com.example.supsup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class home_textinfo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_textinfo);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("상세 내용");
+
+        Button button_chat = (Button) findViewById(R.id.button1);
+
+        button_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), fragment_chat.class);
+                startActivity(intent);
+            } // 채팅 프래그먼트로 가는거 오류남. 채팅 창 액티비티로 바로 이동시키면 될듯
+        });
+
+    }
+}
+
