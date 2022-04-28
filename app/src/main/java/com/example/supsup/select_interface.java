825d920b6d9d62b24fe19a61803ee02dc34f2f1c
dltmdcks98 <dltmdcks98@gmail.com>
2022-04-28 20:44:54 +0900

    승찬 : 인터페이스 화면진입시 TTS기능 안내 멘트 추가 오류 : 기본 볼륨과 안내 멘트 소리 크기가 다름

--- app/src/main/java/com/example/supsup/select_interface.java
+++ app/src/main/java/com/example/supsup/select_interface.java
@@ -1,25 +1,48 @@
 package com.example.supsup;
 
+import static android.speech.tts.TextToSpeech.ERROR;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class select_interface extends AppCompatActivity {
-    Button btndefault ,btntts, btnwide;
+    Button btndefault, btnwide;
+    TextToSpeech tts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_interface);
-
+        //TTS 생성
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+        final Handler handler = new Handler();
         ActionBar ab = getSupportActionBar();
         ab.setTitle("화면 선택");
         btnwide=(Button) findViewById(R.id.btn_wide);
         btndefault=(Button) findViewById(R.id.btn_default);
-        btntts = (Button)findViewById(R.id.btn_tts);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tts.speak("TTS 기능을 사용하려면 크게 보기 버튼을 눌러주세요",TextToSpeech.QUEUE_FLUSH,null);
+            }
+        },3000);
         btndefault.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,13 +57,16 @@ public class select_interface extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        btntts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Main_TTS.class);
-                startActivity(intent);
-            }
-        });
-    }
 
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //TTS 객체 제거
+        if(tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
 }
