fbb051fb9ca144a348d2466b10147f84532ab433
adorable-Kihwan <qkrrlghks20@naver.com>
2022-06-03 18:49:51 +0900

    dd

--- app/src/main/java/com/example/supsup/fragment_home_helpme.java
+++ app/src/main/java/com/example/supsup/fragment_home_helpme.java
@@ -249,6 +249,7 @@ public class fragment_home_helpme extends Fragment {
                         customTextViewHolder.textView1.setText(textModelList.get(position).title);
                         customTextViewHolder.textView2.setText(textModelList.get(position).address);
                         customTextViewHolder.textView3.setText(textModelList.get(position).name);
+                        customTextViewHolder.textView4.setText(textModelList.get(position).suptegory);
 
             }
 
@@ -262,6 +263,7 @@ public class fragment_home_helpme extends Fragment {
                 TextView textView1;
                 TextView textView2;
                 TextView textView3;
+                TextView textView4;
 
                 public CustomTextViewHolder(@NonNull View itemView) {
                     super(itemView);
@@ -269,6 +271,7 @@ public class fragment_home_helpme extends Fragment {
                     this.textView1 = itemView.findViewById(R.id.item_text_title);
                     this.textView2 = itemView.findViewById(R.id.item_text_location);
                     this.textView3 = itemView.findViewById(R.id.item_text_name);
+                    this.textView4 = itemView.findViewById(R.id.item_text_suptegory);
 
                     itemView.setOnClickListener(new View.OnClickListener() {
                         @Override
--- app/src/main/java/com/example/supsup/fragment_home_helpyou.java
+++ app/src/main/java/com/example/supsup/fragment_home_helpyou.java
@@ -254,7 +254,7 @@ public class fragment_home_helpyou extends Fragment {
                 customTextViewHolder.textView1.setText(textModelList.get(position).title);
                 customTextViewHolder.textView2.setText(textModelList.get(position).address);
                 customTextViewHolder.textView3.setText(textModelList.get(position).name);
-
+            customTextViewHolder.textView4.setText(textModelList.get(position).suptegory);
 
         }
 
@@ -268,6 +268,7 @@ public class fragment_home_helpyou extends Fragment {
             TextView textView1;
             TextView textView2;
             TextView textView3;
+            TextView textView4;
 
             public CustomTextViewHolder(@NonNull View itemView) {
                 super(itemView);
@@ -275,7 +276,7 @@ public class fragment_home_helpyou extends Fragment {
                 this.textView1 = itemView.findViewById(R.id.item_text_title);
                 this.textView2 = itemView.findViewById(R.id.item_text_location);
                 this.textView3 = itemView.findViewById(R.id.item_text_name);
-
+                this.textView4 = itemView.findViewById(R.id.item_text_suptegory);
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
